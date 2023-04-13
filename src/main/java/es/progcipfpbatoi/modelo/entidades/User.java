7ae7de19aa1fec31f33284db09ab4eb675cbc869
Fran Gregori Tandazo <fran@MacBook-Pro-de-Fran-2.local>
2023-04-13 13:52:46 +0200

    ADD codigo postal user

--- src/main/java/es/progcipfpbatoi/modelo/entidades/User.java
+++ src/main/java/es/progcipfpbatoi/modelo/entidades/User.java
@@ -1,11 +1,14 @@
 package es.progcipfpbatoi.modelo.entidades;
 
+import java.time.LocalDate;
+
 public class User {
     private String        nombre;
     private String        apellidos;
     private Dni           dni;
     private Correo        email;
     private TelefonoMovil telefonoMovil;
-    private int           fechaNacimiento;
+    private LocalDate     fechaNacimiento;
+    private CodigoPostal codigoPostal;
     private Password      password;
 }
